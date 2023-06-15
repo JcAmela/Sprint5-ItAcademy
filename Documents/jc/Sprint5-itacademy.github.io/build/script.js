ea4d995502d5ae7612270fd81fcaafccd2122c9a
JcAmela <juancarlosamela@gmail.com>
2023-06-15 10:55:03 +0200

    nivel 1 terminado

--- Documents/jc/Sprint5-itacademy.github.io/build/script.js
+++ Documents/jc/Sprint5-itacademy.github.io/build/script.js
@@ -1,12 +1,13 @@
-//*** DOM ***//
+// DOM //
 
 let boton = document.getElementById("boton")
 let respuesta = document.getElementById("respuesta")
-let saveObjects = [];
-let ranking = [];
+let reportAcudits = [];
+let reportJokes = [];
+let contenido_div = document.getElementById("scores_button")
 boton.onclick = peticionApi
 
-//*** async function / petición API ***//
+// async function / petición API //
 async function peticionApi() {
 
     try {
@@ -28,24 +29,45 @@ async function peticionApi() {
 
 }
 
-//  Pinta el chiste de la api
+//  Pinta el chiste de la api y manda ejecutar la funcion de pintar botones del score unicamente si no estan pintados ya.
 function mostrar_chiste(mostrarChiste) {
     respuesta.innerHTML = mostrarChiste
+
+    // esto es para que no me pinte los 3 botones todo el rato que le doy al boton de siguiente
+    if(contenido_div.innerHTML.trim() === "") { //trim es para evitar los espacios en blanco, saltos de linea etc...
+        pintar_botones();
+    }
 }
 
-// creo el array de objetos: saveObjects que guarda todos los objetos obtenidos por la api
+//  Pinta los botones del score
+function pintar_botones(){
+    contenido_div.innerHTML =
+    `<button id="like" class="col-3" onclick="asignar_score(1)"><i class="fas fa-smile fa-2x"></i></button>
+    <button id="igual" class="col-3" onclick="asignar_score(2)"><i class="fas fa-meh fa-2x"></i></button>
+    <button id="dissLike" class="col-3" onclick="asignar_score(3)"><i class="fas fa-frown fa-2x"></i></button>`
+}
+
+
+// creo el array de objetos:  reportAcudits que guarda todos los objetos obtenidos por la api
 function guardarDatos(datos) {
-    saveObjects.push(datos)
+    reportAcudits.push(datos)
 }
 
 // se le asigna el score al nuevo objeto y se le sobrescribe el score al anterior, y tambien lo borra
 function asignar_score(clicado) {
-    let ultimo_objeto = saveObjects[saveObjects.length - 1];
+    let ultimo_objeto =  reportAcudits[ reportAcudits.length - 1];
     if (ultimo_objeto.score === undefined) {
         let obj = { ...ultimo_objeto }
         obj.score = clicado
-        ranking = ranking.filter(o => o.id != obj.id)
-        ranking.push(obj)
+        reportJokes = reportJokes.filter(o => o.id != obj.id)
+        reportJokes.push(obj)
     }
-    console.log(ranking)
+    fecha_valoracion()
+}
+function fecha_valoracion(){
+    let asignarfecha =  reportJokes[ reportJokes.length - 1]
+    let fechaNueva = new Date().toISOString();
+    asignarfecha.date = fechaNueva
+    console.log(reportJokes)
+    return reportJokes
 }
