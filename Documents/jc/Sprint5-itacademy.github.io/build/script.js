c169664391d77bbf5026cf9a92bfb333197078d0
JcAmela <juancarlosamela@gmail.com>
2023-06-19 09:45:07 +0200

    reparado los botones de SCORE

--- Documents/jc/Sprint5-itacademy.github.io/build/script.js
+++ Documents/jc/Sprint5-itacademy.github.io/build/script.js
@@ -6,7 +6,7 @@ let respuesta = document.getElementById("respuesta")
 let contenido_div = document.getElementById("scores_button")
 boton.onclick = chistesItAcademyApi;
 
-// petición a la api de chistes proporcionado por It Academy //
+// Función asincrónica que se encarga de hacer una petición a la API de chistes proporcionada por It Academy.
 async function chistesItAcademyApi() {
 
     try {
@@ -25,7 +25,7 @@ async function chistesItAcademyApi() {
 
 }
 
-//  Pinta el chiste de la api y manda ejecutar la funcion de pintar botones del score unicamente si no estan pintados ya.
+// Función encargada de mostrar el chiste obtenido desde la API en el elemento HTML de respuesta. También verifica si los botones de puntuación ya están pintados en la pantalla para no repetirlos.
 function mostrar_chiste(mostrarChiste) {
     respuesta.innerHTML = mostrarChiste
 
@@ -35,29 +35,21 @@ function mostrar_chiste(mostrarChiste) {
     }
 }
 
-//  Pinta los botones del score ademas de asignarle el valor del boton clicado al evento onclic
+// Función que se encarga de generar el contenido HTML para los botones de puntuación de los chistes y asignarles el respectivo evento onclick.
 function pintar_botones() {
     contenido_div.innerHTML =
-        `<button id="like" class="col-3"><i class="fas fa-smile fa-2x"></i></button>
-        <button id="igual" class="col-3"><i class="fas fa-meh fa-2x"></i></button>
-        <button id="dissLike" class="col-3"><i class="fas fa-frown fa-2x"></i></button>`
-
-
-        // esto no esta bien
-    document.getElementById('like').addEventListener('onclick', () => asignar_score(1));
-    document.getElementById('igual').addEventListener('onclick', () => asignar_score(2));
-    document.getElementById('dissLike').addEventListener('onclick', () => asignar_score(3));
+        `<button id="like" class="col-3" onclick="asignar_score(1)" ><i class="fas fa-smile fa-2x"></i></button>
+        <button id="igual" class="col-3" onclick="asignar_score(2)" ><i class="fas fa-meh fa-2x"></i></button>
+        <button id="dissLike" class="col-3" onclick="asignar_score(3)" ><i class="fas fa-frown fa-2x"></i></button>`
 }
 
-
-
-// creo el array de objetos:  reportAcudits que guarda todos los objetos obtenidos por la api
+// Función encargada de guardar el chiste obtenido de la API en un array, además de invocar la función para mostrar este chiste en pantalla.
 function guardarDatos(datos) {
     reportAcudits.push(datos)
     mostrar_chiste(datos.joke)
 }
 
-// se le asigna el score al nuevo objeto y se le sobrescribe el score al anterior, y tambien lo borra
+// Función que se encarga de asignar una puntuación a un chiste específico. Si el chiste ya tiene una puntuación, esta se sobrescribe con el nuevo valor.
 function asignar_score(clicado) {
     let ultimo_objeto = reportAcudits[reportAcudits.length - 1];
     if (ultimo_objeto.score === undefined) {
@@ -68,6 +60,8 @@ function asignar_score(clicado) {
     }
     fecha_valoracion()
 }
+
+// Función que se encarga de asignar la fecha actual a la puntuación de un chiste en particular. Luego imprime en consola el array de chistes puntuados.
 function fecha_valoracion() {
     let asignarfecha = reportJokes[reportJokes.length - 1]
     let fechaNueva = new Date().toISOString();
@@ -75,6 +69,8 @@ function fecha_valoracion() {
     console.log(reportJokes)
     return reportJokes
 }
+
+// Función asincrónica que se encarga de hacer una petición a una API para obtener información sobre el clima actual.
 async function apiWeather() {
     const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=84.123.164.93';
     const options = {
@@ -93,6 +89,8 @@ async function apiWeather() {
         console.error(error);
     }
 }
+
+// Función que se encarga de mostrar en pantalla la información sobre el clima actual obtenida de la API.
 apiWeather()
 function tiempo(informacion) {
     document.getElementById("tiempo").innerHTML=`Hoy en ${informacion.location.name} hace una temperatura de: ${informacion.current.temp_c}ºC a tiempo real. <br> Según la información proporcionada por: https://rapidapi.com/weatherapi.`
